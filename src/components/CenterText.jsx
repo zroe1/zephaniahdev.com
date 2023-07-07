41a9544d490236c80deff55964dd0e8649c45130
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-07-07 10:40:15 -0500

    changed center quote

--- src/components/CenterText.jsx
+++ src/components/CenterText.jsx
@@ -6,15 +6,15 @@ import ClickHereButton from './ClickHereButton'
 const CenterText = (props) => {
   return (
     <div className='text-container' style={{color: props.color}}>
-      <h1>Zephaniah Roe</h1>
+      <h1 className='bold'>Zephaniah Roe</h1>
       {/* <p className='interests'>python | tensorflow | systems</p> */}
       <p className='quote'>
-      “Simplicity, carried to the extreme, becomes elegance.”
-        {/* "The internet? We are not intested in it." */}
+      {/* “Simplicity, carried to the extreme, becomes elegance.” */}
+        "The internet? We are not intested in it."
         {/* <br />
         -Bill Gates, 1993 */}
         {/* <br /> */}
-        {/* <div className='quote-credit'>-Bill Gates, 1993</div> */}
+        <div className='quote-credit'>-Bill Gates, 1993</div>
       </p>
 
       <ClickHereButton buttonText='CLICK HERE' />
