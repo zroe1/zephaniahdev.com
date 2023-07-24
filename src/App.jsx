0fe8f915ec59b11f5999268ef3684fe2d88eb3c8
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-07-24 16:30:43 -0500

    added rotation to click here button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei'
 import CenterText from './components/CenterText'
 import ColorSelectors from './components/ColorSelector';
@@ -7,19 +7,33 @@ import InstructionText from './components/InstructionText';
 import BottomLinks from './components/BottomLinks';
 import './index.css'
 
-function Box({color}) {
+function Box({color, rotation}) {
+  const [totalRotation, setTotalRotation] = useState(0);
+
+  const setRotation = (newRotation) => {
+    setTotalRotation(totalRotation + newRotation)
+  }
+
   const meshRef = useRef()
   useFrame(() => {
     if (!meshRef.current) {
       return;
     }
-    // meshRef.current.rotation.y += 0.006;
+    meshRef.current.rotation.y += rotation;
+    setRotation(rotation);
+    console.log(totalRotation);
   })
+
+  //  sets the arguments for the mesh depending on the window size
   let meshArgs;
   if (window.innerWidth < 600) {
     meshArgs = [2.0, .6, 16, 100];
   } else {
     meshArgs = [2.2, .7, 16, 100];
+  }
+
+  if (totalRotation > 9.5) {
+    return null;
   }
   return (
     <mesh ref={meshRef}>
@@ -33,6 +47,7 @@ function Box({color}) {
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("#000000")
   const [ShapeColor, setShapeColor] = useState("#ffffff")
+  const [ShapeRotation, setShapeRotation] = useState(0)
   const [TextColor, setTextColor] = useState("#ffffff")
 
   const setBackground  = (newBackgroundColor) => {
@@ -44,23 +59,35 @@ export default function App() {
   const setText = (newTextColor) => {
     setTextColor(newTextColor)
   }
+  const setRotation = (newRotation) => {
+    console.log("I am here!!");
+    setShapeRotation(newRotation)
+  }
   const backgroundStyle = {
     backgroundColor: backgroundColor,
     height: window.innerHeight,
   }
+  const centerTextStyle = {
+    display: 'none',
+  }
+  // if (ShapeRotation != 0) {
+  //   centerTextStyle = {
+  //     display: 'none',
+  //   }
+  // }
 
   return (
     <div className='root-container' style={backgroundStyle}>
       <InstructionText color={TextColor}/>
       <BottomLinks />
       <ColorSelectors color={TextColor} handleBackgroundChange={setBackground} handleShapeChange={setShape} handleTextChange={setText}/>
-      <CenterText color={TextColor}/>
+      {ShapeRotation == 0 && <CenterText color={TextColor} handleRotationChange={setRotation}/>}
       <Canvas >
         <OrbitControls />
         <Stars/>
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 15, 10]} angle={0.3} />
-        <Box color={ShapeColor}/>
+        <Box color={ShapeColor} rotation={ShapeRotation}/>
       </Canvas>
     </div>
   )
--- src/components/CenterText.jsx
+++ src/components/CenterText.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useState } from "react"
 import './CenterText.css'
+import './ClickHereButton.css'
 import ClickHereButton from './ClickHereButton'
 
 
 const CenterText = (props) => {
+  const [rotation, setRotation] = useState(0);
+  console.log("I am here");
+
+  const changeRotation = () => {
+    console.log("I am here!");
+    setRotation(0.3);
+    props.handleRotationChange(0.3);
+  }
+
   return (
     <div className='text-container' style={{color: props.color}}>
       <h1 className='bold'>Zephaniah Roe</h1>
-      {/* <p className='interests'>python | tensorflow | systems</p> */}
       <p className='quote'>
-      {/* “Simplicity, carried to the extreme, becomes elegance.” */}
         "The internet? We are not intested in it."
-        {/* <br />
-        -Bill Gates, 1993 */}
-        {/* <br /> */}
         <div className='quote-credit'>-Bill Gates, 1993</div>
       </p>
 
-      <ClickHereButton buttonText='CLICK HERE' />
+      <button className='center-button' onClick={changeRotation}>CLICK HERE</button>
     </div>
   )
 }
