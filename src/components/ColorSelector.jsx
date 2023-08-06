60987b8bdf42d99f126e3776e7e4e565ef8f37e8
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-06 17:58:54 -0500

    fixed bug where 'Settings' text is too small on mobile

--- src/components/ColorSelector.jsx
+++ src/components/ColorSelector.jsx
@@ -24,7 +24,7 @@ const ColorSelectors = (props) => {
   return (
     <div className='settings'>
       <p className='instruction-text' style={{color: props.color}}>Press down and drag to move through space.</p>
-      <p style={{color: props.color}}>Settings:</p>
+      <p className='settings-string' style={{color: props.color}}>Settings:</p>
       <div className='inputs'>
         <input type="color" id="Background" name="Background" value={backgroundColor} onChange={changeBackground} />
         <label htmlFor="Background">Background</label>
