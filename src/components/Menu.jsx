c63d3d53d45167e880a94f0ac37228b5a523a770
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-06 19:01:29 -0500

    built out menu fade animation and added menu text

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,9 +5,15 @@ const Menu = (props) => {
 
   return (
     <div className="test">
-      <div className="menu-item"></div>
-      <div className="menu-item"></div>
-      <div className="menu-item"></div>
+      <div className="menu-item" id="portfolio">
+        Portfolio
+      </div>
+      <div className="menu-item" id="resume">
+        Resume
+      </div>
+      <div className="menu-item" id="productivity">
+        Productivity
+      </div>
     </div>
   )
 
