7aea2da0e4a571f7157e9cbd0c4b983baf02490f
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-15 17:22:32 -0500

    refactored menu css for readability

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,32 +11,29 @@ const Menu = (props) => {
 
   return (
     <div>
-      {!isPortfolioSelected && <div className="test">
-        <div className="menu-item" id="portfolio" onClick={redeclareIsPortfolioSelected}>
+      {!isPortfolioSelected && <div className="menu-container">
+        <div className="menu-item animationFirst" onClick={redeclareIsPortfolioSelected}>
           Portfolio
         </div>
-        <a className="menu-item"  id="resume" href="https://docs.google.com/document/d/13v9_JG2TeXQJmx2OnifMUYJrlVzoWrazk1Un0m4iyGE/edit?usp=sharing" target="_blank">
+        <a className="menu-item animationSecond" href="https://docs.google.com/document/d/13v9_JG2TeXQJmx2OnifMUYJrlVzoWrazk1Un0m4iyGE/edit?usp=sharing" target="_blank">
             Resume
         </a>
-        <div className="menu-item" id="productivity">
+        <div className="menu-item animationThird" >
           Productivity
           <p id="productivity-coming-soon">coming soon...</p>
         </div>
       </div>}
 
-      {isPortfolioSelected && <div className="test">
-        {/* <a className="menu-item"  id="portfolio" href="https://github.com/zroe1" target="_blank">
-          Github
-        </a> */}
-        <PortfolioItem id="portfolio" itemName={"tinyfilter"} 
+      {isPortfolioSelected && <div className="menu-container">
+        <PortfolioItem  animationNum="animationFirst" itemName={"tinyfilter"} 
                         status={"Version 1.0.1 (stable release 8/7/2023)"}
                         discription={"tinyfilter converts images into ASCII art using the principles of CNNs (convolutional neural networks)."}
                         link={"https://github.com/zroe1/tinyfilter"}/>
-        <PortfolioItem id="resume" itemName={"roestories"}
+        <PortfolioItem animationNum="animationSecond" itemName={"roestories"}
                         status={"Completed for release on 7/19/2023"}
                         discription={"a portfolio website for my dad, featuring all his work from the over 30 years he spent in journalism."}
                         link={"https://roestories.com/"}/>
-        <PortfolioItem id="productivity" itemName={"MIT 6.S191"}
+        <PortfolioItem animationNum="animationThird" itemName={"MIT 6.S191"}
                         status={"Completed August 9, 2023"}
                         discription={"I took this class (Introduction to Deep Learning) durring the 2023 summer to self-study AI and neural networks."}
                         link={"https://github.com/zroe1/MIT-6.S191"}/>
--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -3,7 +3,7 @@ import '../Menu.css'
 
 const PortfolioItem = (props) => {
     return (
-        <a className="portfolio-item" id={props.id} href={props.link} target="_blank">
+        <a className={`portfolio-item ${props.animationNum}`} href={props.link} target="_blank">
             <h2 className="menu-title">{props.itemName}</h2>
             <div className="portfolio-divider"></div>
             <p><span className='portfolio-item-bold'>Status:</span> {props.status}</p>
