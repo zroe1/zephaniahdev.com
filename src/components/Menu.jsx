9b0a67fc68b079f3c5ac5aedf0ed670f1c6f73e4
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-17 16:03:19 -0500

    built out third page of menu and more responsive menu controls

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -17,6 +17,9 @@ const Menu = (props) => {
     if (menuPage == 1) {
         setMenuPage(0)
     }
+    if (menuPage == 2) {
+      setMenuPage(1)
+    }
   }
 
   const switchRight = () => {
@@ -24,6 +27,9 @@ const Menu = (props) => {
     if (menuPage == 0) {
         setMenuPage(1)
     }
+    if (menuPage == 1) {
+      setMenuPage(2)
+  }
   }
 
   return (
@@ -33,42 +39,13 @@ const Menu = (props) => {
           Portfolio
         </div>
         <a className="menu-item animationSecond" href="https://docs.google.com/document/d/13v9_JG2TeXQJmx2OnifMUYJrlVzoWrazk1Un0m4iyGE/edit?usp=sharing" target="_blank">
-            Resume
+          Resume
         </a>
         <div className="menu-item animationThird" >
           Productivity
           <p id="productivity-coming-soon">coming soon...</p>
         </div>
       </div>}
-
-      {isPortfolioSelected && menuPage == 1 && <div className="menu-container">
-        {/* <PortfolioItem  animationNum="animationFirst" itemName={"tinyfilter"} 
-                        status={"Version 1.0.1 (stable release 8/7/2023)"}
-                        discription={"tinyfilter converts images into ASCII art using the principles of CNNs (convolutional neural networks)."}
-                        link={"https://github.com/zroe1/tinyfilter"}/>
-        <PortfolioItem animationNum="animationSecond" itemName={"roestories"}
-                        status={"Completed for release on 7/19/2023"}
-                        discription={"A portfolio website for my dad, featuring all his work from the over 30 years he spent in journalism."}
-                        link={"https://roestories.com/"}/>
-        <PortfolioItem animationNum="animationThird" itemName={"MIT 6.S191"}
-                        status={"Completed August 9, 2023"}
-                        discription={"I took this class (Introduction to Deep Learning) durring the 2023 summer to self-study AI and neural networks."}
-                        link={"https://github.com/zroe1/MIT-6.S191"}/> */}
-
-        <PortfolioItem  animationNum="animationFirst" itemName={"Recreating 2048"} 
-                        status={"Completed 6/12/2023"}
-                        discription={"Using python I created a TUI for the popular game 2048 and wrote an algorithm that beats the game ~30% of the time."}
-                        link={"https://github.com/zroe1/tinyfilter"}/>
-        <PortfolioItem animationNum="animationSecond" itemName={"Letter predictor"}
-                        status={"Completed for release on 6/13/2023"}
-                        discription={"Using a linear regression model and publicly available data, my letter predictor predicts the final letter in five-letter words."}
-                        link={"https://roestories.com/"}/>
-        <PortfolioItem animationNum="animationThird" itemName={"zephysballoons.com"}
-                        status={"Completed 7/18/23"}
-                        discription={"zephysballoons.com is a very simple personal website for my balloon twisting business."}
-                        link={"https://github.com/zroe1/zephysballoons.com"}/>
-      </div>}
-
       {isPortfolioSelected && menuPage == 0 && <div className="menu-container">
         <PortfolioItem  animationNum="animationFirst" itemName={"tinyfilter"} 
                         status={"Version 1.0.1 (stable release 8/7/2023)"}
@@ -83,10 +60,38 @@ const Menu = (props) => {
                         discription={"I took this class (Introduction to Deep Learning) durring the 2023 summer to self-study AI and neural networks."}
                         link={"https://github.com/zroe1/MIT-6.S191"}/>
       </div>}
+      {isPortfolioSelected && menuPage == 1 && <div className="menu-container">
+        <PortfolioItem  animationNum="animationFirst" itemName={"Recreating 2048"} 
+                        status={"Completed 6/12/2023"}
+                        discription={"Using python I created a TUI for the popular game 2048 and wrote an algorithm that beats the game ~30% of the time."}
+                        link={"https://github.com/zroe1/tinyfilter"}/>
+        <PortfolioItem animationNum="animationSecond" itemName={"Letter predictor"}
+                        status={"Completed for release on 6/13/2023"}
+                        discription={"Using a linear regression model and publicly available data, my letter predictor predicts the final letter in five-letter words."}
+                        link={"https://roestories.com/"}/>
+        <PortfolioItem animationNum="animationThird" itemName={"zephysballoons.com"}
+                        status={"Completed 7/18/23"}
+                        discription={"zephysballoons.com is a very simple personal website for my balloon twisting business."}
+                        link={"https://github.com/zroe1/zephysballoons.com"}/>
+      </div>}
+      {isPortfolioSelected && menuPage == 2 && <div className="menu-container">
+        <PortfolioItem  animationNum="animationFirst" itemName={"Productivity Website"} 
+                        status={"Still in progress"}
+                        discription={"tinyfilter converts images into ASCII art using the principles of CNNs (convolutional neural networks)."}
+                        link={"https://github.com/zroe1/tinyfilter"}/>
+        <PortfolioItem animationNum="animationSecond" itemName={"Sorting: Rust vs C"}
+                        status={"Still in progress"}
+                        discription={"A repository where I write common sorting algorithms in Rust and C and compare their preformance."}
+                        link={"https://github.com/zroe1/sorting-rust-c"}/>
+        <PortfolioItem animationNum="animationThird" itemName={"Find unused files"}
+                        status={"Completed 7/17/2023"}
+                        discription={"Python script that recursivly searchs through directories in a website and tracks references to files to determine which can be safely deleted."}
+                        link={"https://github.com/zroe1/https://github.com/zroe1/find-unused-files-web-dev-6.S191"}/>
+      </div>}
 
       {isPortfolioSelected && <>
-            <RightArrow handleSwitchRight={switchRight}/>
-            <LeftArrow handleSwitchLeft={switchLeft}/>
+            {menuPage != 2 && <RightArrow handleSwitchRight={switchRight}/>}
+            {menuPage != 0 && <LeftArrow handleSwitchLeft={switchLeft}/>}
         </>}
     </div>
   )
