6dbee3a20f0d715c3e54b77cedc1c82a95e7c4e8
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-17 14:56:40 -0500

    removed unused code

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import './Button.css'
 
-// href="https://zephyroe.github.io"
-
 const Button = (props) => {
   return (
     <a className='button' href={props.link} target="_blank">{props.buttonText}</a>
